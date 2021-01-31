96d9f218d6089d42c1d6d101f1bdc3aaec0f2c20
mainzelM <mainzelm@gmail.com>
2021-01-31 21:03:28 +0100

    Test for automatic network mode

--- eblocker-icapserver/src/main/java/org/eblocker/server/http/service/DoctorService.java
+++ eblocker-icapserver/src/main/java/org/eblocker/server/http/service/DoctorService.java
@@ -1,13 +1,30 @@
 package org.eblocker.server.http.service;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import org.eblocker.server.common.data.NetworkConfiguration;
+import org.eblocker.server.common.network.NetworkServices;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @Singleton
 public class DoctorService {
+
+    private final NetworkServices networkServices;
+
+    @Inject
+    public DoctorService(NetworkServices networkServices) {
+        this.networkServices = networkServices;
+    }
+
     public List<String> runDiagnosis() {
-        return Arrays.asList("Diag 1", "Diag 2", "Diag 3");
+        List<String> problems = new ArrayList<>();
+
+        NetworkConfiguration currentNetworkConfiguration = networkServices.getCurrentNetworkConfiguration();
+        if (currentNetworkConfiguration.isAutomatic()) {
+            problems.add("You are using the automatic mode, consider switching to expert mode");
+        }
+        return problems;
     }
 }
