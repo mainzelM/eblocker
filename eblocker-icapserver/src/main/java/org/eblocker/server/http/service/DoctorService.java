5dd4910527e2674bc354ebed8032b17191da6cb8
mainzelM <mainzelm@gmail.com>
2021-02-12 16:34:16 +0100

    Add a couple of dummy diagnosis

--- eblocker-icapserver/src/main/java/org/eblocker/server/http/service/DoctorService.java
+++ eblocker-icapserver/src/main/java/org/eblocker/server/http/service/DoctorService.java
@@ -10,7 +10,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.eblocker.server.common.data.DoctorDiagnosisResult.Audience.EVERYONE;
+import static org.eblocker.server.common.data.DoctorDiagnosisResult.Audience.EXPERT;
+import static org.eblocker.server.common.data.DoctorDiagnosisResult.Audience.NOVICE;
+import static org.eblocker.server.common.data.DoctorDiagnosisResult.Severity.ANORMALY;
+import static org.eblocker.server.common.data.DoctorDiagnosisResult.Severity.FAILED_PROBE;
 import static org.eblocker.server.common.data.DoctorDiagnosisResult.Severity.HINT;
+import static org.eblocker.server.common.data.DoctorDiagnosisResult.Severity.RECOMMENDATION_NOT_FOLLOWED;
 
 @Singleton
 public class DoctorService {
@@ -30,6 +35,31 @@ public class DoctorService {
             problems.add(new DoctorDiagnosisResult(HINT, EVERYONE, "You are using the automatic network mode. It may cause problems."));
         }
 
+        problems.add(new DoctorDiagnosisResult(FAILED_PROBE, EVERYONE, "IPv6 seems to be enabled in your network. Please turn it off if HTTPS is used"));
+
+        problems.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, NOVICE, "HTTPS is not enabled. You will get better tracking protection with it"));
+
+        problems.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "Automatic mode is not enabled for device XY"));
+
+        problems.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "eBlocker will be automatically enabled for new devices"));
+
+        problems.add(new DoctorDiagnosisResult(HINT, EXPERT, "The Auto Trust App is not enabled"));
+
+        problems.add(new DoctorDiagnosisResult(HINT, EXPERT, "The DDGTR blocker list is not enabled"));
+
+        problems.add(new DoctorDiagnosisResult(HINT, EXPERT, "The cookie blocker list is not enabled"));
+
+        problems.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "Malware & Phishing Blocker list is not enabled globally"));
+
+        problems.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "Malware & Phishing Blocker list is not enabled for device XY"));
+
+        problems.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "Control bar is not auto-configured for device XY"));
+
+        problems.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "Automatic updates are disabled"));
+
+        problems.add(new DoctorDiagnosisResult(FAILED_PROBE, EVERYONE, "Last system update is older than 24 hours"));
+
+        problems.add(new DoctorDiagnosisResult(ANORMALY, EVERYONE, "Child XY has no restrictions"));
         return problems;
     }
 }
