3b918a304e54cbecd4ce25ea043dfebbb44fbe20
mainzelM <mainzelm@gmail.com>
2021-02-25 22:39:00 +0100

    Add ping(6) diagnosis

--- eblocker-icapserver/src/main/java/org/eblocker/server/http/service/DoctorService.java
+++ eblocker-icapserver/src/main/java/org/eblocker/server/http/service/DoctorService.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.eblocker.server.common.data.DoctorDiagnosisResult.Audience.EVERYONE;
 import static org.eblocker.server.common.data.DoctorDiagnosisResult.Audience.EXPERT;
@@ -46,69 +47,79 @@ public class DoctorService {
     }
 
     public List<DoctorDiagnosisResult> runDiagnosis() {
-        List<DoctorDiagnosisResult> problems = new ArrayList<>();
+        List<DoctorDiagnosisResult> diagnoses = new ArrayList<>();
 
         NetworkConfiguration currentNetworkConfiguration = networkServices.getCurrentNetworkConfiguration();
         if (currentNetworkConfiguration.isAutomatic()) {
-            problems.add(new DoctorDiagnosisResult(HINT, EVERYONE, "You are using the automatic network mode. It may cause problems."));
+            diagnoses.add(new DoctorDiagnosisResult(HINT, EVERYONE, "You are using the automatic network mode. It may cause problems."));
         } else {
-            problems.add(new DoctorDiagnosisResult(GOOD, EVERYONE, "You are using a good network mode"));
+            diagnoses.add(new DoctorDiagnosisResult(GOOD, EVERYONE, "You are using a good network mode"));
+        }
+
+        if (pingHost(4, "eblocker.org")) {
+            diagnoses.add(new DoctorDiagnosisResult(GOOD, EVERYONE, "Your eBlocker can reach the internet via ICMP"));
+        } else {
+            diagnoses.add(new DoctorDiagnosisResult(FAILED_PROBE, EVERYONE, "Your eBlocker cannot reach the internet"));
         }
 
         if (sslService.isSslEnabled()) {
-            problems.add(new DoctorDiagnosisResult(GOOD, EVERYONE, "You have HTTPS enabled"));
+            diagnoses.add(new DoctorDiagnosisResult(GOOD, EVERYONE, "You have HTTPS enabled"));
 
-            problems.add(new DoctorDiagnosisResult(HINT, EXPERT, "FAKE: The Auto Trust App is not enabled"));
+            diagnoses.add(new DoctorDiagnosisResult(HINT, EXPERT, "FAKE: The Auto Trust App is not enabled"));
 
-            problems.add(new DoctorDiagnosisResult(HINT, EXPERT, "FAKE: The DDGTR blocker list is not enabled"));
+            diagnoses.add(new DoctorDiagnosisResult(HINT, EXPERT, "FAKE: The DDGTR blocker list is not enabled"));
 
-            problems.add(new DoctorDiagnosisResult(HINT, EXPERT, "FAKE: The cookie blocker list is not enabled"));
+            diagnoses.add(new DoctorDiagnosisResult(HINT, EXPERT, "FAKE: The cookie blocker list is not enabled"));
 
-            problems.add(new DoctorDiagnosisResult(FAILED_PROBE, EVERYONE, "FAKE: IPv6 seems to be enabled in your network. Please turn it off as you are using HTTPS"));
+            if (pingHost(6, "google.com")) {
+                diagnoses.add(new DoctorDiagnosisResult(FAILED_PROBE, EVERYONE, "You have IPv6 enabled. That will bypass the tracking of eBlocker"));
+            } else {
+                diagnoses.add(new DoctorDiagnosisResult(GOOD, EVERYONE, "You have IPv6 disabled"));
+            }
 
         } else {
-            problems.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EXPERT, "HTTPS is not enabled. You will get better tracking protection with it"));
+            diagnoses.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EXPERT, "HTTPS is not enabled. You will get better tracking protection with it"));
         }
 
-        problems.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "FAKE: Automatic mode is not enabled for device XY"));
+        diagnoses.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "FAKE: Automatic mode is not enabled for device XY"));
 
         if (deviceFactory.isAutoEnableNewDevices()) {
-            problems.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "eBlocker will be automatically enabled for new devices. This may cause trouble when a new device is not ready for eBlocker"));
+            diagnoses.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "eBlocker will be automatically enabled for new devices. This may cause trouble when a new device is not ready for eBlocker"));
         } else {
-            problems.add(new DoctorDiagnosisResult(GOOD, EVERYONE, "eBlocker will not be automatically enabled for new devices, so you don't run into trouble during setup. Don't forget to enable new devices manually..."));
+            diagnoses.add(new DoctorDiagnosisResult(GOOD, EVERYONE, "eBlocker will not be automatically enabled for new devices, so you don't run into trouble during setup. Don't forget to enable new devices manually..."));
         }
 
-        problems.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "FAKE: Malware & Phishing Blocker list is not enabled globally for Domain Blocking"));
+        diagnoses.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "FAKE: Malware & Phishing Blocker list is not enabled globally for Domain Blocking"));
 
-        problems.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "FAKE: Malware & Phishing Blocker list is not enabled globally for Pattern Blocking"));
+        diagnoses.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "FAKE: Malware & Phishing Blocker list is not enabled globally for Pattern Blocking"));
 
-        problems.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "FAKE: Malware & Phishing Blocker list is not enabled for device XY"));
+        diagnoses.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "FAKE: Malware & Phishing Blocker list is not enabled for device XY"));
 
-        problems.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "FAKE: Control bar is not auto-configured for device XY"));
+        diagnoses.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "FAKE: Control bar is not auto-configured for device XY"));
 
         if (automaticUpdater.isActivated()) {
-            problems.add(new DoctorDiagnosisResult(GOOD, EVERYONE, "Your eBlocker is configured to automatically update itself on a daily basis"));
+            diagnoses.add(new DoctorDiagnosisResult(GOOD, EVERYONE, "Your eBlocker is configured to automatically update itself on a daily basis"));
         } else {
-            problems.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "Automatic updates are disabled"));
+            diagnoses.add(new DoctorDiagnosisResult(RECOMMENDATION_NOT_FOLLOWED, EVERYONE, "Automatic updates are disabled"));
         }
 
         LocalDateTime lastUpdateTime = debianUpdater.getLastUpdateTime();
         if (lastUpdateTime == null) {
-            problems.add(new DoctorDiagnosisResult(FAILED_PROBE, EVERYONE, "System updates never ran"));
+            diagnoses.add(new DoctorDiagnosisResult(FAILED_PROBE, EVERYONE, "System updates never ran"));
         } else if (LocalDateTime.now().minusDays(2).isBefore(lastUpdateTime)) {
-            problems.add(new DoctorDiagnosisResult(FAILED_PROBE, EVERYONE, "Last system update is older than two days : " + lastUpdateTime));
+            diagnoses.add(new DoctorDiagnosisResult(FAILED_PROBE, EVERYONE, "Last system update is older than two days : " + lastUpdateTime));
         } else {
-            problems.add(new DoctorDiagnosisResult(GOOD, EVERYONE, "System updates are okay"));
+            diagnoses.add(new DoctorDiagnosisResult(GOOD, EVERYONE, "System updates are okay"));
         }
 
-        problems.add(new DoctorDiagnosisResult(ANORMALY, EVERYONE, "FAKE: Child XY has no restrictions"));
+        diagnoses.add(new DoctorDiagnosisResult(ANORMALY, EVERYONE, "FAKE: Child XY has no restrictions"));
 
         if (hasNonGoodNameServers()) {
-            problems.add(new DoctorDiagnosisResult(FAILED_PROBE, EVERYONE, "You have name servers with non-good rating"));
+            diagnoses.add(new DoctorDiagnosisResult(FAILED_PROBE, EVERYONE, "You have name servers with non-good rating"));
         } else {
-            problems.add(new DoctorDiagnosisResult(GOOD, EVERYONE, "Your name servers look good"));
+            diagnoses.add(new DoctorDiagnosisResult(GOOD, EVERYONE, "Your name servers look good"));
         }
-        return problems;
+        return diagnoses;
     }
 
     private boolean hasNonGoodNameServers() {
@@ -116,5 +127,18 @@ public class DoctorService {
                 ZonedDateTime.now().minusHours(24).toInstant()).getNameServerStats();
         // TODO: deal with empty stat list
         return nameServerStats.stream().anyMatch(nss -> !nss.getRating().equals(DnsRating.GOOD));
+    }
+
+    private boolean pingHost(int ipVersion, String hostName) {
+        String pingCommand = ipVersion == 4 ? "ping" : "ping6";
+        ProcessBuilder pb = new ProcessBuilder(pingCommand, "-c", "1", hostName);
+        try {
+            Process start = pb.start();
+            boolean result = start.waitFor(5, TimeUnit.SECONDS);
+            return result && start.exitValue() == 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
